fbc4f91122b135a4c7ea6ef732113b28e6d2841b
Andrew Hany <andrewdodz51234@gmail.com>
2021-06-18 14:25:21 +0200

    added helpers.js

---
+++ src/Components/PollCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { formatQuestion, formatDate } from '../Utils/api'
+import { Link, withRouter } from 'react-router-dom'
+
+class PollCard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
